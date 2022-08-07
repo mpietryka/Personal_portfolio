e32f108bb3bfccfa3617ea3f2c119fccbe20e340
mpietryka <mat.pietryka@gmail.com>
2022-08-07 20:11:59 +0100

    minor styling fixes
    i have fixed the issue of the grid of technologies not displaying properly on ultrawide screens. i have also change the text colour to white on the blue bubbles.

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -8,18 +8,18 @@ export const Bio = ({bio}) => {
             <div className='flex justify-center mb-4'>
                 <img src={profilePic} alt="logo" className='rounded-full hover:scale-105 transition duration-300 ease-in-out'/>
             </div>
-            <div className=' max-w-sm md:max-w-md md:mx-16'>
+            <div className='max-w-sm md:max-w-lg md:mx-16'>
                 <h1 className='text-xl md:text-3xl text-left font-semibold'>About me:</h1>
-                <p className='text-justify text-sm md:text-md font-light pt-2'>My name is Mateusz Pietryka, I am 29 years old and I am a soon to be a 3rd year Computer Science Student. 
-                I am very interested in Front-end Design and Development. 
+                <p className='text-justify text-sm md:text-md font-light pt-2'>My name is Mateusz Pietryka, I am 29 years old and I am a soon to be a 3rd year Computer 
+                Science Student. I am very interested in Front-end Design and Development. 
                 </p>
-                <p className='text-justify text-sm md:text-md font-light pt-2'>I have discovered my passion for programming not long before Covid-19 pandemic and
-                I decided to pursue this as my career. After a short research about the available courses, I have applied to London Metropolitan Unviersity 
-                via clearing and started my course within 2 months. 
+                <p className='text-justify text-sm md:text-md font-light pt-2'>I have discovered my passion for programming not long before Covid-19 pandemic started and
+                I decided to pursue this as my career. After a short research about the available courses, I have applied to London Metropolitan Unviersity via clearing 
+                and started my course within 2 months. 
                 </p>
-                <p className='text-justify text-sm md:text-md font-light pt-2'>As the time passes, I feel like I have made the best decision of my life, studying Computer Science 
-                taught me much more than just programming languages, it taught me critical thinking, problem solving and that I can achieve anything if I try hard enough.
-
+                <p className='text-justify text-sm md:text-md font-light pt-2'>As the time passes, I feel like I have made the best decision of my life, studying Computer 
+                Science taught me much more than just programming languages, it taught me critical thinking, problem solving and that I can achieve anything if I try hard 
+                enough.
                 </p>
             </div>
         </div>
