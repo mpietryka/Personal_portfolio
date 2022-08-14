7d253baf31266cc551bbe79cf1238f19f092c32b
mpietryka <mat.pietryka@gmail.com>
2022-08-14 17:48:48 +0100

    .

--- src/containers/carousel/Carousel.js
+++ src/containers/carousel/Carousel.js
@@ -1,38 +1,22 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { MainContainer } from "../../components";
+import { MainContainer, Heading } from "../../components";
 
 export const Carousel = () => {
   return (
-    <>
-      <MainContainer>
-        <Swiper className="mySwiper">
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 1</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 2</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 3</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 4</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 5</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 6</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 7</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="m-16 text-center">Slide 8</p>
-          </SwiperSlide>
-        </Swiper>
-      </MainContainer>
-    </>
+    <MainContainer>
+      <Heading>Github Repositories</Heading>
+      <Swiper slidesPerView={1} spaceBetween={30} className="mySwiper">
+        <SwiperSlide>
+          <p className="flex m-12 justify-center">Slide 1</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p className="flex m-12 justify-center">Slide 2</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p className="flex m-12 justify-center">Slide 3</p>
+        </SwiperSlide>
+      </Swiper>
+    </MainContainer>
   );
 };
